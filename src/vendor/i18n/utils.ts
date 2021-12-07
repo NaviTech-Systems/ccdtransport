d1bf783d43aaad01095d5c1922d5917003a96b56
Daniel Ionut <dany89yt@yahoo.com>
2021-12-07 13:53:19 +0200

    inital commit

--- src/vendor/i18n/utils.ts
+++ src/vendor/i18n/utils.ts
@@ -1,7 +1,12 @@
 import { derived, get } from 'svelte/store';
 import { languages } from './state';
 
-const createUrlFormatter = (languages) => (url: string) => `/${languages.locale}${url}`;
+const createUrlFormatter = (languages) => (url: string) => {
+	if (url === '/') {
+		return `/${languages.locale}`;
+	}
+	return `/${languages.locale}${url}`;
+};
 
 export const composeUrl = derived([languages], ([$languages]) => {
 	console.log($languages);
