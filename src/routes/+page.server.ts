9550ad59d8065ebb655a8ed99c5f7f29b86a5922
Daniel Ionut <daniel@navitech.systems>
2024-01-18 13:57:19 +0200

    Refactor language redirection in +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,8 +2,6 @@ import { ServerLoad, redirect } from '@sveltejs/kit';
 
 export const load: ServerLoad = ({ request }) => {
 	//get browser language en-GB,en-US;q=0.9,en;q=0.8,ro;q=0.7
-	const lang = request.headers['accept-language']?.split(',')?.[0]?.split('-')?.[0] || 'en';
-	//redirect to the language page
 
-	throw redirect(307, lang === 'ro' ? '/ro' : '/en');
+	throw redirect(307, '/en');
 };
